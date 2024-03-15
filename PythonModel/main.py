86dcf257ecb3ed43e40e5f8b8b1fbaf17bb50521
Ben Fawthrop <benjamin.fawthrop@gmail.com>
2024-03-15 16:26:33 -0400

    Added some other implementations which I tried for various tests, each explained in comments above in main.py

--- PythonModel/main.py
+++ PythonModel/main.py
@@ -35,3 +35,114 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+"""
+Attempt 2: 
+
+# this attempt was an attempt at taking in all the variables we 
+# wanted to use before actually learning how to use the API
+# There were several errors with trying to actually use the inputs, so I scrapped it and started fresh
+import pybamm
+
+def run_battery_model(cell_type, max_voltage, min_voltage, nom_voltage, 
+                      nom_internal_resistance, max_current, ambient_temp, 
+                      current_draw):
+    # Create a PyBaMM parameter set for the given cell type
+    chemistry = pybamm.parameter_sets.Chen2020
+    parameter_values = pybamm.ParameterValues(chemistry=chemistry, values={})
+
+
+    # Set user-defined parameters
+    parameter_values.update(
+        {
+            "Maximum voltage [V]": max_voltage,
+            "Minimum voltage [V]": min_voltage,
+            "Nominal cell voltage [V]": nom_voltage,
+            "Nominal internal resistance [Ohm]": nom_internal_resistance,
+            "Maximum current [A]": max_current,
+            "Ambient temperature [K]": ambient_temp,
+            "Current function [A]": current_draw,
+        }
+    )
+
+    # Create the battery model
+    model = pybamm.lithium_ion.DFN()
+
+    # Set parameter values
+    model.parameter_values = parameter_values
+
+    # Solve the model
+    solution = pybamm.CasadiSolver().solve(model)
+
+    # Extract desired outputs
+    battery_temp = solution["X-averaged cell temperature [K]"](0)
+    internal_resistance = solution["X-averaged total heating [W]"](0) / (current_draw ** 2)
+
+    return battery_temp, internal_resistance
+
+def main():
+    cell_type = input("Input a cell type: ")
+    max_voltage = float(input("Input a maximum cell voltage: "))
+    min_voltage = float(input("Input a minimum cell voltage: "))
+    nom_voltage = float(input("Input a nominal cell voltage: "))
+    nom_internal_resistance = float(input("Input a nominal internal resistance: "))
+    max_current = float(input("Input a maximum current: "))
+    ambient_temp = float(input("Input an ambient temperature: "))
+    current_draw = float(input("Input a current draw from load: "))
+
+    battery_temp, internal_resistance = run_battery_model(cell_type, max_voltage, 
+                                                          min_voltage, nom_voltage, 
+                                                          nom_internal_resistance, 
+                                                          max_current, ambient_temp, 
+                                                          current_draw)
+
+    print("Battery Temperature:", battery_temp, "K")
+    print("Internal Resistance:", internal_resistance, "Ohm")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+# Our eventual implementation of the package we are building
+# is to incorporate it into our lap sim.  Unfortunately, the API we are using doesnt have 
+# the functionality to feed us real time data over an ongoing simulation (at least that I have found yet,
+# however I'm still looking).  Therefore, I was testing to see how long it would take to check the battery 
+# status at each time step, to see if it was impractical to just do it that way.  My conclusion is that the 
+# solution wouldn't be that elegant, but also isn't too slow, so as a last resort we can do it that way.
+Attempt 3:
+import pybamm
+
+# Define the battery model
+model = pybamm.lithium_ion.DFN()
+
+# Create a simulation object
+sim = pybamm.Simulation(model)
+
+# Define simulation time and time steps
+t_eval = np.linspace(0, race_time, num_time_steps)
+
+# Iterate over time steps
+for t in t_eval:
+    # Set current profile based on race conditions
+    current = get_current_profile(t)
+    
+    # Set inputs for the simulation
+    sim.specs["operating mode"] = "current"
+    sim.specs["current function"] = current
+    
+    # Solve the model for the current time step
+    sim.solve(t_eval=[t, t + dt])
+    
+    # Collect data at each time step
+    voltage = sim.solution["Terminal voltage"]
+    temperature = sim.solution["Cell temperature"]
+    
+    # Perform analysis or visualization with the collected data
+
+# Visualize results or perform further analysis
+
+"""
