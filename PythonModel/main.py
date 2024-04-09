9a425467e4b62847b3177938566fc222b60b52ca
Ben Fawthrop <benjamin.fawthrop@gmail.com>
2024-04-09 17:29:59 -0400

    Figured out my problem, shifted to fixing steady state model.  Once done, will continue with LapSim model

--- PythonModel/main.py
+++ PythonModel/main.py
@@ -5,185 +5,87 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def test_model():
-    start = time()
+# Function to get user input with default values
+def get_input(prompt, default):
+    while True:
+        user_input = input(prompt)
+        if user_input == '':
+            return default
+        try:
+            return eval(user_input)
+        except SyntaxError:
+            print("Invalid input, please try again.")
+
+def single_cell_model(amb, surf, cell_vol, init_temp, cap, min_volt, max_volt, time_to_run):
+    """
+    Runs a battery model on a single cell based on the following given inputs and plots them
+
+    :param amb: Ambient Temp in Kelvin
+    :param surf: Cell cooling surface area in meters sqrd
+    :param cell_vol: Cell volume in meters cubed
+    :param init_temp: Initial Temp in Kelvin
+    :param cap: Nominal Cell capacity in amp hours
+    :param min_volt: minimum voltage in volts
+    :param max_volt: maximum voltage in volts
+    :param time_to_run: Time over which to run the model in minutes
+    :return: none
+    """
+    # sets up model
     options = {"thermal": "x-full"}
     model = pybamm.lithium_ion.DFN(options)
-    solns = []
 
+    # sets up parameters
     param = model.default_parameter_values
-    param['Ambient temperature [K]'] = 316.483
-    param['Cell cooling surface area [m2]'] = 0.00367
-    param['Cell volume [m3]'] = 1.65e-05
-    param['Initial temperature [K]'] = 316.483
-    param['Nominal cell capacity [A.h]'] = 2.6
-    param['Open-circuit voltage at 0% SOC [V]'] = 2.5
-    param['Open-circuit voltage at 100% SOC [V]'] = 4.2
-    param['Current function [A]'] = 2
-
-    sim = pybamm.Simulation(model, parameter_values=param)
-
-    t_eval = np.linspace(0, 3600)
-    sim.solve(t_eval)
-
-    end = time()-start
-
-    solns.append(sim._solution)
-
-
-
-    # Now create a QuickPlot object with the adjusted solutions
-    quick_plot = pybamm.QuickPlot(solns, ["X-averaged cell temperature [K]"])
-    quick_plot.dynamic_plot()
-
-    # print(solns[0]["Cell temperature [K]"].entries)
-    # print(solns[0]["Voltage [V]"].entries)
-    # print(solns[0].get_data_dict())
-
-    # plt.plot(solns[0]["Voltage [V]"].entries)
-
-
-    # Plot the solutions dynamically
-    # new_plot = pybamm.dynamic_plot(solns, ["X-averaged cell temperature [K]"])
-    print(type(quick_plot.variables[("X-averaged cell temperature [K]",)][0][0].base_variables[0]))
-    '''
-    <class 'pybamm.expression_tree.binary_operators.Addition'>
-    
-    This is the type of the data printed above, so thats where you will find the class (i.e. the binary_operators
-    file in the PyBaMM folders)
-    '''
-
-
-
-    # quick_plot.plots[("Voltage [V]", )][0][0].show()
-
-    plt.show()
-    print(end)
-
-
-def main():
-    # cell_type = 18650  # input("Input a cell type: ")
-    # max_voltage = 4.2  # float(input("Input a maximum cell voltage: "))
-    # min_voltage = 2.5  # float(input("Input a minimum cell voltage: "))
-    # nom_voltage = 3.6  # float(input("Input a nominal cell voltage: "))
-    # max_current = 36  # float(input("Input a maximum current: "))
-    # nom_internal_resistance = 20  # float(input("Input a nominal internal resistance: "))
-    # ambient_temp = 40  # float(input("Input an ambient temperature: "))
-    # current_draw = 12.87  # float(input("Input a current draw from load: "))
-
-    test_model()
-
-
-if __name__ == "__main__":
-    main()
-
-
-"""
-Attempt 2: 
-
-# this attempt was an attempt at taking in all the variables we 
-# wanted to use before actually learning how to use the API
-# There were several errors with trying to actually use the inputs, so I scrapped it and started fresh
-import pybamm
-
-def run_battery_model(cell_type, max_voltage, min_voltage, nom_voltage, 
-                      nom_internal_resistance, max_current, ambient_temp, 
-                      current_draw):
-    # Create a PyBaMM parameter set for the given cell type
-    chemistry = pybamm.parameter_sets.Chen2020
-    parameter_values = pybamm.ParameterValues(chemistry=chemistry, values={})
-
-
-    # Set user-defined parameters
-    parameter_values.update(
+    param.update(
         {
-            "Maximum voltage [V]": max_voltage,
-            "Minimum voltage [V]": min_voltage,
-            "Nominal cell voltage [V]": nom_voltage,
-            "Nominal internal resistance [Ohm]": nom_internal_resistance,
-            "Maximum current [A]": max_current,
-            "Ambient temperature [K]": ambient_temp,
-            "Current function [A]": current_draw,
+            'Ambient temperature [K]': amb,
+            'Cell cooling surface area [m2]': surf,
+            'Cell volume [m3]': cell_vol,
+            'Initial temperature [K]': init_temp,
+            'Nominal cell capacity [A.h]': cap,
+            'Open-circuit voltage at 0% SOC [V]': min_volt,
+            'Open-circuit voltage at 100% SOC [V]': max_volt,
+            # "Current function [A]": "[input]",
         }
     )
 
-    # Create the battery model
-    model = pybamm.lithium_ion.DFN()
+    # builds sim
+    sim = pybamm.Simulation(model, parameter_values=param)
 
-    # Set parameter values
-    model.parameter_values = parameter_values
+    # creates an array of time steps to run over
+    t_eval = np.linspace(0, time_to_run * 60)
+    sim.solve(t_eval)
 
-    # Solve the model
-    solution = pybamm.CasadiSolver().solve(model)
+    # print(sim.solution["X-averaged cell temperature [K]"].entries)
+    # print(sim.solution["Time [s]"].entries)
 
-    # Extract desired outputs
-    battery_temp = solution["X-averaged cell temperature [K]"](0)
-    internal_resistance = solution["X-averaged total heating [W]"](0) / (current_draw ** 2)
+    # # get the SOC values
+    # soc = sim.solution['State of Charge']
+    #
+    # # calculate the time at which SOC reaches 0%
+    # discharge_time = sim.solution['Time [s]'].entries[soc.entries.argmin()]
+    #
+    # print("Discharge Time:", discharge_time / 60, "minutes")
 
-    return battery_temp, internal_resistance
+    # solve and plot
+    quick_plot = pybamm.QuickPlot(sim, ["X-averaged cell temperature [K]", "Voltage [V]",
+                                        "Total heating [W.m-3]", "Current [A]", ])
+    quick_plot.dynamic_plot()
+
 
 def main():
-    cell_type = input("Input a cell type: ")
-    max_voltage = float(input("Input a maximum cell voltage: "))
-    min_voltage = float(input("Input a minimum cell voltage: "))
-    nom_voltage = float(input("Input a nominal cell voltage: "))
-    nom_internal_resistance = float(input("Input a nominal internal resistance: "))
-    max_current = float(input("Input a maximum current: "))
-    ambient_temp = float(input("Input an ambient temperature: "))
-    current_draw = float(input("Input a current draw from load: "))
+    amb_temp = get_input("Ambient temperature [K] (default is 316.483): ", 316.483)
+    surf_area = get_input("Cell cooling surface area [m2] (default is 0.00367): ", 0.00367)
+    cell_vol = get_input("Cell volume [m3] (default is 1.65e-05): ", 1.65e-05)
+    init_temp = get_input("Initial temperature [K] (default is 316.483): ", 316.483)
+    nom_cap = get_input("Nominal cell capacity [A.h] (default is 2.6): ", 2.6)
+    min_volt = get_input("Open-circuit voltage at 0% SOC [V] (default is 2.5): ", 2.5)
+    max_volt = get_input("Open-circuit voltage at 100% SOC [V] (default is 4.2): ", 4.2)
+    t = get_input("Enter a number of minutes to run for (default is 60 mins): ", 60)
 
-    battery_temp, internal_resistance = run_battery_model(cell_type, max_voltage, 
-                                                          min_voltage, nom_voltage, 
-                                                          nom_internal_resistance, 
-                                                          max_current, ambient_temp, 
-                                                          current_draw)
+    single_cell_model(amb_temp, surf_area, cell_vol, init_temp, nom_cap, min_volt, max_volt, t)
 
-    print("Battery Temperature:", battery_temp, "K")
-    print("Internal Resistance:", internal_resistance, "Ohm")
 
 if __name__ == "__main__":
     main()
 
-
-
-
-
-# Our eventual implementation of the package we are building
-# is to incorporate it into our lap sim.  Unfortunately, the API we are using doesnt have 
-# the functionality to feed us real time data over an ongoing simulation (at least that I have found yet,
-# however I'm still looking).  Therefore, I was testing to see how long it would take to check the battery 
-# status at each time step, to see if it was impractical to just do it that way.  My conclusion is that the 
-# solution wouldn't be that elegant, but also isn't too slow, so as a last resort we can do it that way.
-Attempt 3:
-import pybamm
-
-# Define the battery model
-model = pybamm.lithium_ion.DFN()
-
-# Create a simulation object
-sim = pybamm.Simulation(model)
-
-# Define simulation time and time steps
-t_eval = np.linspace(0, race_time, num_time_steps)
-
-# Iterate over time steps
-for t in t_eval:
-    # Set current profile based on race conditions
-    current = get_current_profile(t)
-    
-    # Set inputs for the simulation
-    sim.specs["operating mode"] = "current"
-    sim.specs["current function"] = current
-    
-    # Solve the model for the current time step
-    sim.solve(t_eval=[t, t + dt])
-    
-    # Collect data at each time step
-    voltage = sim.solution["Terminal voltage"]
-    temperature = sim.solution["Cell temperature"]
-    
-    # Perform analysis or visualization with the collected data
-
-# Visualize results or perform further analysis
-
-"""
